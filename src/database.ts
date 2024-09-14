62444a20fcb8f7225fff98fd5484b1e4d3e95398
Hsin-Wen Chang <bow1226@gmail.com>
2024-09-14 15:40:06 +0800

    feat: Update Use the appropriate database based on the ENV variable

--- src/database.ts
+++ src/database.ts
@@ -3,17 +3,26 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV,
+} = process.env;
 
 class Database {
   private static instance: Database;
   private client: Pool;
 
   private constructor() {
+    // Use the appropriate database based on the ENV variable
+    const database = ENV === "test" ? POSTGRES_TEST_DB : POSTGRES_DB;
+
     this.client = new Pool({
       host: POSTGRES_HOST,
-      database: POSTGRES_DB,
+      database: database,
       user: POSTGRES_USER,
       password: POSTGRES_PASSWORD,
     });
