9ec1613b2e8cd20dea0b7b9b1b80c0276b4e3760
Hsin-Wen Chang <bow1226@gmail.com>
2024-10-26 16:41:57 +0800

    fix: relation orders already exists

--- migrations/sqls/20241019102103-orders-up.sql
+++
@@ -1,8 +0,0 @@
-/* Replace with your SQL commands */
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE, -- Foreign key to User
-    total_amount DECIMAL(10, 2) NOT NULL, -- Total amount of the order
-    customer_name VARCHAR(100) NOT NULL, -- Name of the customer
-    status VARCHAR(50) NOT NULL CHECK (status IN ('pending', 'completed')) -- Restricting status values
-);
