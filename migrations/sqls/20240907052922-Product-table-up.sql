80fa6f234acbb60e6e2cf4d834e62ed84991385f
Hsin-Wen Chang <bow1226@gmail.com>
2024-12-02 13:21:53 +0800

    test:  Insert dummy data

--- migrations/sqls/20240907052922-Product-table-up.sql
+++ migrations/sqls/20240907052922-Product-table-up.sql
@@ -5,3 +5,7 @@ CREATE TABLE products (
     price DECIMAL(10, 2) NOT NULL, -- Using DECIMAL for currency
     description TEXT
 );
+
+-- Insert some products
+INSERT INTO products (name, price, description) VALUES ('Product 1', 100.00, 'Description of Product 1');
+INSERT INTO products (name, price, description) VALUES ('Product 2', 50.00, 'Description of Product 2');
